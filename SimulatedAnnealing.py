c187357e0e65caf54212a05d55339a2df3ea44f3
DiegoAloslla <diegoalosilla@gmail.com>
2019-09-08 22:52:22 -0500

    agregar comentarios

---
+++ SimulatedAnnealing.py
@@ -0,0 +1,48 @@
+import random
+import math
+import pygame
+from Points import Points
+class SimulatedAnnealing:
+    #Inicialzamos las varibales
+    def __init__(self, destinos, temperatura_inicial, valocidad_enfriamiento):
+            self.points_admin = destinos
+            self.points = Points(destinos)
+            self.points.generar_points()
+            self.el_mejor = self.points
+            self.temperatura = temperatura_inicial
+            self.valocidad_enfriamiento = valocidad_enfriamiento
+
+    #Valida si la enregia es menor  o mayor
+    def funcion_aceptacion(self, delta_energia):
+        if delta_energia < 0:
+            return True
+        elif random.random() <= math.exp(-(delta_energia / self.temperatura)):
+            return True
+        return False
+
+    #Creamos un nuevo punto de coordenadsd para compararlo con elactual
+    def nuevo_points(self):
+        points_nuevo = Points(self.points_admin, self.points)
+
+        pos1 = random.randrange(self.points.point_size())
+        pos2 = random.randrange(self.points.point_size())
+        ciudad1 = points_nuevo.get_coordinates(pos1)
+        ciudad2 = points_nuevo.get_coordinates(pos2)
+        points_nuevo.set_coordinates(pos2, ciudad1)
+        points_nuevo.set_coordinates(pos1, ciudad2)
+
+        actual_energia = self.points.get_distance()
+        nueva_energia = points_nuevo.get_distance()
+        delta = nueva_energia - actual_energia
+
+        if self.funcion_aceptacion(delta):
+            self.points = points_nuevo
+
+        if points_nuevo.get_distance() < self.el_mejor.get_distance():
+            self.el_mejor = points_nuevo
+
+    #cooremos mediante un ciclo la funcion nuevo point y actualozamos  la temperatura
+    def run(self):
+        while self.temperatura > 1:
+            self.nuevo_points()
+            self.temperatura *= 1 - self.valocidad_enfriamiento
